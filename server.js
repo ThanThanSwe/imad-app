c306958325d9f93773cce35e8944b8c6b78479c7
ThanThanSwe <30908928+ThanThanSwe@users.noreply.github.com>
2017-08-29 02:05:55 +0630

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+var crypto=require('crypto');
 
+var config={ user : 'thanthanswetz',
+            database : '	thanthanswessstz',
+            password : 'db-thanthanswessstz-87856',
+            host: 'ssh.imad.hasura-app.io',
+            port : '5432' }
+            
 var app = express();
 app.use(morgan('combined'));
-
+function createTemplate(data)
+{
+    }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash(input,salt){
+    //how do we create a hash?
+    var hashed=crypto.pbkdf2Sync(input,salt,1000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input',function(req,res)
+{
+    var hashedString=hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
+    });
+    var pool=new Pool(config);
+    app.get('/test-db',function(req,res)
+    {
+       pool.query('Select * from test',function(err,result){
+           if(err){
+               res.status(500).send(err.toString());
+           }
+           else{
+               res.send(JSON.stringify(result.rows));
+           }
+       });
+    });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
